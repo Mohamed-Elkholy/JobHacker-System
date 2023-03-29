fe22be32981649ec9ee162208265331151a0e6a9
Mohamed-Elkholy <mohamed315374@gmail.com>
2023-03-29 02:07:50 +0200

    Task 1 added

---
+++ Task1/src/Main.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt() , b = s.nextInt();
+        int sum = a + b ;
+        System.out.println("X = " + sum);
+    }
+}
