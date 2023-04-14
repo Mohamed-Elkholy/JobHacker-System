88b1782975341b1e93fea9dd7caf3491bd4e0a33
Mohamed-Elkholy <mohamed315374@gmail.com>
2023-04-14 07:42:46 +0200

    tasks have been collected in one file called Week1&2

---
+++ Week1&2/Task1/src/Main.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class Main {
+
+    public static int sum(int num1 , int num2){
+        return num1 + num2 ;
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num1 = s.nextInt() , num2 = s.nextInt();
+        System.out.println("X = " + sum(num1 , num2));
+    }
+}
---
+++ Week1&2/Task12/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.* ;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in) ;
+        Formatter f = new Formatter();
+        double[][] matrix = new double[12][12];
+        double row = s.nextDouble() , sum = 0 ;
+        char operation = s.next().charAt(0);
+        for (int i = 0 ; i < 12 ; i++) {
+            for (int j = 0 ; j < 12 ; j++) {
+                matrix[i][j] = s.nextDouble() ;
+                if(i == row) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+
+        if(operation == 'S') {
+            System.out.println(f.format("%.1f" , sum));
+        }else{
+            System.out.println(f.format("%.1f" , (sum / 12.0)));
+        }
+
+    }
+}
