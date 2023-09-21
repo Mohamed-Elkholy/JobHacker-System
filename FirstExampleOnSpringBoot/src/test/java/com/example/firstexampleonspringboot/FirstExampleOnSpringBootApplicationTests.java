dfb68111f3a69961163a7dea026f83aa2dfc379c
Mohamed-Elkholy <82127362+Mohamed-Elkholy@users.noreply.github.com>
2023-09-21 23:51:11 +0300

    all example

---
+++ FirstExampleOnSpringBoot/src/test/java/com/example/firstexampleonspringboot/FirstExampleOnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstexampleonspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstExampleOnSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
